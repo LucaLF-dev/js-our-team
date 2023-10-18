615ad15bc0e5894bf70815532d52297893cef4de
LucaLF-dev <laficaraluca@gmail.com>
2023-10-18 18:00:26 +0200

    Modify: delete some lines of code js

--- js/app.js
+++ js/app.js
@@ -6,72 +6,58 @@ const containerElement = document.querySelector('.container')
 console.log(containerElement)
 
 const team = [
-    {
-        nome : 'Wayne Barnett',
-        ruolo:'Founder & CEO',	
-        foto: 'wayne-barnett-founder-ceo.jpg',
-    },
-    
-    {
-        nome :'Angela Caroll',
-        ruolo:'Chief Editor',
-        foto:'angela-caroll-chief-editor.jpg',
-    },
+	{
+		nome: "Wayne Barnett",
+		ruolo: "Founder & CEO",
+		foto: "wayne-barnett-founder-ceo.jpg",
+	},
 
-    {
-        nome :'Walter Gordon',
-        ruolo:'Office Manager',	
-        foto: 'walter-gordon-office-manager.jpg',
-    },
+	{
+		nome: "Angela Caroll",
+		ruolo: "Chief Editor",
+		foto: "angela-caroll-chief-editor.jpg",
+	},
 
-    {
-        nome :'Angela Lopez',
-        ruolo:'Social Media Manager',
-        foto:'angela-lopez-social-media-manager.jpg',
-    },
+	{
+		nome: "Walter Gordon",
+		ruolo: "Office Manager",
+		foto: "walter-gordon-office-manager.jpg",
+	},
 
-    {
-        nome :'Scott Estrada',
-        ruolo:'Developer	',
-        foto: 'scott-estrada-developer.jpg',
-    },
+	{
+		nome: "Angela Lopez",
+		ruolo: "Social Media Manager",
+		foto: "angela-lopez-social-media-manager.jpg",
+	},
 
-    {
-        nome :'Barbara Ramos',
-        ruolo:'Graphic Designer',	
-        foto: 'barbara-ramos-graphic-designer.jpg',
-    },
+	{
+		nome: "Scott Estrada",
+		ruolo: "Developer	",
+		foto: "scott-estrada-developer.jpg",
+	},
 
-   
-    
-]
-console.log(team)
+	{
+		nome: "Barbara Ramos",
+		ruolo: "Graphic Designer",
+		foto: "barbara-ramos-graphic-designer.jpg",
+	},
+];
+console.log(team);
 
-const teamMember = []
-console.log(teamMember)
+let teamMemberAll = "";
 
 // stampo nella console per ogni membro del team le sue informazione
-// - ciclo l'array 
+// - ciclo l'array
 for (i = 0; i < team.length; i++) {
-// - ottengo singolarmente gli elementi dell' array
-    const currentInformation = team[i]
-    console.log(currentInformation)
-// - creo una variabile information con ltre informazioni nome,ruolo,immaggine
-   const informationTeamMember = `${currentInformation.nome} ${currentInformation.ruolo} ${currentInformation.foto}`
-// pusho le stringhe in un nuovo array di nome teamMember   
-   teamMember.push(informationTeamMember)
-// - stampo in console
-      console.log(informationTeamMember)
-
-    containerElement.innerHTML += `<div class = "card> <figure class= "role-member">${currentInformation.foto}</figure> <div class= "information-member"><span>${currentInformation.nome}</span><span> ${currentInformation.ruolo}<span></div></div>`
+	// - ottengo singolarmente gli elementi dell' array
+	const currentInformation = team[i];
+	console.log(currentInformation);
+	// - creo una variabile information con ltre informazioni nome,ruolo,immaggine
+	informationTeamMember = `${currentInformation.nome} ${currentInformation.ruolo} ${currentInformation.foto}`;
+	teamMemberAll += informationTeamMember;
+	// - stampo in console
+	console.log(informationTeamMember);
+	// stampo nel dom le stringhe con le informazioni dei singoli membri
+	containerElement.innerHTML += `<div class = "card> <figure class= "role-member">${currentInformation.foto}</figure> <div class= "information-member"><span>${currentInformation.nome}</span><span> ${currentInformation.ruolo}<span></div></div>`;
 }
-
-// // ciclo l'array team member 
-// for (i = 0; i < teamMember.length; i++) {
-//     const currentMember = teamMember[i]
-//     console.log(currentMember)
-
-//     containerElement.innerHTML += 
-
-// }
-
+console.log(teamMemberAll);
